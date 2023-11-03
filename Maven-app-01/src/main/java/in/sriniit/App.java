cdeab3b8c4bd31627848942a8993692de346f94c
Resh-star <shaikreshma20001109@gmail.com>
2023-11-03 23:44:09 +0530

    1001 changes

--- Maven-app-01/src/main/java/in/sriniit/App.java
+++ Maven-app-01/src/main/java/in/sriniit/App.java
@@ -7,7 +7,9 @@ package in.sriniit;
 public class App 
 {
     public static void main( String[] args )
+    //changes related to 1001
     {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello DevOps" );
     }
 }
